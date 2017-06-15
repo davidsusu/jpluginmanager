96113b325c91995ebafd0fda29dde951eada88ad
Horváth Dávid <horvath@webarticum.hu>
2017-06-15 05:10:25 +0200

    Add SimplePluginLoader

--- jpluginmanager/src/hu/webarticum/jpluginmanager/Main.java
+++ jpluginmanager/src/hu/webarticum/jpluginmanager/Main.java
@@ -4,10 +4,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import hu.webarticum.jpluginmanager.core.CompositePluginLoader;
+import hu.webarticum.jpluginmanager.core.Plugin;
 import hu.webarticum.jpluginmanager.core.PluginLoader;
 import hu.webarticum.jpluginmanager.core.PluginManager;
+import hu.webarticum.jpluginmanager.core.SimplePluginLoader;
 import hu.webarticum.jpluginmanager.loader.jar.DefaultJarPluginLoader;
 import hu.webarticum.jpluginmanager.loader.js.DefaultJsPluginLoader;
 
@@ -17,6 +21,7 @@ public class Main {
         System.out.print("Plugin dir (see resources/sample for test plugins): ");
         String path = new BufferedReader(new InputStreamReader(System.in)).readLine();
         PluginLoader pluginLoader = new CompositePluginLoader(
+            new SimplePluginLoader(SampleInnerPlugin.class),
             new DefaultJarPluginLoader(new File(path)),
             new DefaultJsPluginLoader(new File(path))
         );
@@ -31,5 +36,46 @@ public class Main {
         public void hello();
         
     }
-    
+
+    public static class SampleInnerPlugin implements Plugin {
+
+        @Override
+        public String getName() {
+            return "hu.webarticum.jpluginmanager.SampleInnerPlugin";
+        }
+
+        @Override
+        public String getVersion() {
+            return "0.1.1";
+        }
+
+        @Override
+        public String getLabel() {
+            return "Sample Inner Plugin";
+        }
+
+        @Override
+        public boolean validate() {
+            return true;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> List<T> getExtensions(Class<T> type) {
+            List<T> extensions = new ArrayList<T>();
+            if (type == HelloExtensionInterface.class) {
+                extensions.add((T)new HelloExtensionInterface() {
+                    
+                    @Override
+                    public void hello() {
+                        System.out.println("Hello, Inner!");
+                    }
+                    
+                });
+            }
+            return extensions;
+        }
+        
+    }
+
 }
---
+++ jpluginmanager/src/hu/webarticum/jpluginmanager/core/SimplePluginLoader.java
@@ -0,0 +1,37 @@
+package hu.webarticum.jpluginmanager.core;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class SimplePluginLoader implements PluginLoader {
+    
+    private final List<Class<? extends Plugin>> pluginClasses;
+    
+    @SafeVarargs
+    public SimplePluginLoader(Class<? extends Plugin>... pluginClasses) {
+        this(Arrays.asList(pluginClasses));
+    }
+
+    public SimplePluginLoader(Collection<Class<? extends Plugin>> pluginClasses) {
+        this.pluginClasses = new ArrayList<Class<? extends Plugin>>(pluginClasses);
+    }
+    
+    @Override
+    public List<PluginContainer> loadAll() {
+        List<PluginContainer> pluginContainers = new ArrayList<PluginContainer>();
+        for (Class<? extends Plugin> pluginClass: pluginClasses) {
+            try {
+                Plugin plugin = pluginClass.newInstance();
+                pluginContainers.add(new PluginContainer(pluginClass.getClassLoader(), plugin));
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return pluginContainers;
+    }
+
+}

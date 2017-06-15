74024b68c0440996e2c6c2de0ca254d3fe7ddc77
Horváth Dávid <horvath@webarticum.hu>
2017-06-15 05:29:35 +0200

    Add skeleton of PluginResolver

---
+++ jpluginmanager/src/hu/webarticum/jpluginmanager/core/PluginResolver.java
@@ -0,0 +1,14 @@
+package hu.webarticum.jpluginmanager.core;
+
+
+public class PluginResolver {
+    
+    // TODO / XXX
+    // PluginContainer.[[try?]resolve|start|stop]() ?
+    // Plugin.[start|stop] ? (lifecycle events)
+    
+    public void resolveAll(PluginManager pluginManager) {
+        // TODO
+    }
+    
+}
